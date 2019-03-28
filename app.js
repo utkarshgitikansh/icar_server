3c28185df15cc871bdc9c4bde69238286c394ca1
utkarshgitikansh <utkarshgitikansh@gmail.com>
2019-03-28 13:14:13 +0530

    64th commit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var querystring = require("querystring");
 var upload = multer();
 const req = require("request");
 var cheerio = require("cheerio");
-var research_page = require("./research_page");
+//var research_page = require("./research_page");
 
 const app = express();
 
@@ -34,6 +34,10 @@ global.tender_name_value = [];
 global.tender_url_value = [];
 global.recruit_name_value = [];
 global.recruit_url_value = [];
+
+global.research_paper_urls = [];
+global.author_name = [];
+global.books_urls = [];
 
 const PORT = process.env.PORT || 8080;
 
@@ -183,6 +187,85 @@ app
           console.log(event_name + "\n");
           console.log(event_url + "\n");
         });
+      }
+    });
+
+    /////////////////
+
+    req("http://www.nrclitchi.org/research-papers", (error, response, html) => {
+      if (!error && response.statusCode == 200) {
+        console.log("So far so good ...");
+
+        ///// Using cheerio to fetch the site details//////
+
+        const $ = cheerio.load(html);
+
+        ///// '$' will be used as a reference to getting all website details ///////
+        ///  Research Paper urls
+
+        console.log("Getting Research Paper urls ..." + "\n\n\n");
+
+        var research_url = {};
+
+        var key1 = "research_papers";
+
+        research_url[key1] = [];
+
+        $(".list-icons-style-3 li a").each((i, el) => {
+          const researchUrl = $(el).attr("href");
+
+          research_url[key1].push(researchUrl);
+        });
+
+        research_paper_urls = research_url;
+        var len = research_paper_urls.research_papers.length;
+
+        ///// getting individual web scraping from each url
+
+        for (let i = 0; i < len; i++) {
+          //console.log(research_paper_urls["research_papers"][i]);
+
+          req(
+            `${research_paper_urls["research_papers"][i]}`,
+            (error, response, html) => {
+              if (!error && response.statusCode == 200) {
+                //console.log(research_paper_urls);
+
+                ///// Using cheerio to fetch the site details//////
+
+                const $ = cheerio.load(html);
+
+                var books_url = {};
+                var author_name = [];
+
+                ///// '$' will be used as a reference to getting all website details ///////
+                ///  Research Paper urls
+                $(".col-md-12 h1").each((i, el) => {
+                  author_name = $(el).text();
+                });
+
+                var key1 = author_name;
+
+                books_url[key1] = [];
+
+                $(".list-ordened-style-3 li").each((i, el) => {
+                  const book_name = $(el)
+                    .text()
+                    .replace(/\s\s+/g, " ");
+
+                  books_url[key1].push(book_name);
+                });
+
+                // books_urls = books_url;
+                //console.log(books_urls);
+                books_urls.push(books_url);
+                console.log("\n\n");
+              }
+            }
+          );
+        }
+
+        console.log("\n\n");
       }
     });
   })
